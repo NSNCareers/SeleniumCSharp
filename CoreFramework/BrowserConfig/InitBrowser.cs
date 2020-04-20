965f4389820a7bd91ac38845912240fe9c6fa177
Ajang Ngwesse <elvisdemoprojects@outlook.com>
2020-04-20 15:02:43 +0100

    changes

--- CoreFramework/BrowserConfig/InitBrowser.cs
+++ CoreFramework/BrowserConfig/InitBrowser.cs
@@ -46,7 +46,7 @@ namespace CoreFramework.BrowserConfig
 
                     case "chrome":
                         chromeOptions = new ChromeOptions();
-                        chromeOptions.AddArgument("no-sandbox");
+                        //chromeOptions.AddArgument("no-sandbox");
                         //chromeOptions.AddArgument("--window-size=1920,1080");
                         //chromeOptions.AddArgument("--start-maximized");
                         break;
